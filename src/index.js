a948faee7525ec23d2ef1789a890209dd45f538b
Joy <416102332@qq.com>
2019-05-15 16:02:34 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,135 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import './index.css';
+import { Form, Input, Icon, Button } from 'antd';
+
+let id = 0;
+
+class DynamicFieldSet extends React.Component {
+  remove = k => {
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    // We need at least one passenger
+    if (keys.length === 1) {
+      return;
+    }
+
+    // can use data-binding to set
+    form.setFieldsValue({
+      keys: keys.filter(key => key !== k),
+    });
+  };
+
+  add = () => {
+    const { form } = this.props;
+    // can use data-binding to get
+    const keys = form.getFieldValue('keys');
+    console.log(form.getFieldsValue() );
+    const nextKeys = keys.concat(id++);
+    // can use data-binding to set
+    // important! notify form to detect changes
+    form.setFieldsValue({
+      keys: nextKeys,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        const { keys, names } = values;
+        console.log('Received values of form: ', values);
+        console.log('Merged values:', keys.map(key => names[key]));
+      }
+    });
+  };
+
+  render() {
+    const { getFieldDecorator, getFieldValue } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 4 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 20 },
+      },
+    };
+    const formItemLayoutWithOutLabel = {
+      wrapperCol: {
+        xs: { span: 24, offset: 0 },
+        sm: { span: 20, offset: 4 },
+      },
+    };
+    getFieldDecorator('keys', { initialValue: [] });
+    const keys = getFieldValue('keys');
+    const prontoId = keys.map((k, index) => (
+      <div>
+      <Form.Item key={`pronto${k}`}
+        {...formItemLayout}
+        label='Pronto ID'
+      >
+        {getFieldDecorator(`names[${k}].prontoId`, {
+          validateTrigger: ['onChange', 'onBlur'],
+          rules: [
+            {
+              required: true,
+              whitespace: true,
+              message: "Please input passenger's name or delete this field.",
+            },
+          ],
+        })(
+          <Input placeholder="passenger name" style={{ width: '60%', marginRight: 8 }} />)}
+        {keys.length > 1 ? (
+          <Icon
+            className="dynamic-delete-button"
+            type="minus-circle-o"
+            onClick={() => this.remove(k)}
+          />
+        ) : null}
+      </Form.Item>
+      <Form.Item key={`desc${k}`}
+          {...formItemLayout}
+          label={'Desc'}
+        >
+          {getFieldDecorator(`names[${k}].desc`, {
+            validateTrigger: ['onChange', 'onBlur'],
+            rules: [
+              {
+                required: false,
+              },
+            ],
+          })(
+            <Input placeholder="desc" style={{ width: '60%', marginRight: 8 }} />)}
+        
+        </Form.Item>
+      </div>
+    ));
+  
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        {prontoId}
+        
+        <Form.Item {...formItemLayoutWithOutLabel}>
+          <Button type="dashed" onClick={this.add} style={{ width: '60%' }}>
+            <Icon type="plus" /> Add field
+          </Button>
+        </Form.Item>
+        <Form.Item {...formItemLayoutWithOutLabel}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
+
+const WrappedDynamicFieldSet = Form.create({ name: 'dynamic_form_item' })(DynamicFieldSet);
+ReactDOM.render(<WrappedDynamicFieldSet />, document.getElementById('container'));
+
+          
